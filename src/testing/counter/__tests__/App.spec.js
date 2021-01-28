5c468c031fecde063935a6dfc6a53be4075d864c
Marc Klefter <marc@remotifi.com>
2021-01-28 11:47:32 +0100

    Added testing example

---
+++ src/testing/counter/__tests__/App.spec.js
@@ -0,0 +1,33 @@
+import {
+  render,
+  screen,
+  waitFor
+} from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+
+import {
+  App
+} from '../App';
+
+test('should increment the counter when the button is clicked', async () => {
+  render(<App />);
+
+  // see if Testing Playground recommends the same queries.
+  screen.debug();
+
+  const increment = screen.getByRole('button', { name: /increment/i });
+  let result = screen.getByText(/count: 0/i);
+
+  expect(result).toHaveTextContent('Count: 0');
+  
+  userEvent.click(increment);
+  expect(result).toHaveTextContent('Count: 1');
+
+  const reset = screen.getByRole('button', { name: /reset/i });
+  userEvent.click(reset);
+
+  await waitFor(() => {
+    expect(result).toHaveTextContent('Count: 0');
+  });
+});
